b3095d3989138e3ddeca02bb314197eccc57f2f5
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-07-07 15:33:00 +0530

    Deleted App.css

--- react-search-filter/src/components/SearchBar.jsx
+++ react-search-filter/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { Container, Input, Text } from "@chakra-ui/react";
 export default function SearchBar() {
   return (
     <Container>
-      <Text fontSize="4xl" as="u" color="gray" textAlign={"center"}>
+      <Text fontSize="4xl" as="u" color="gray" textAlign="center">
         Hello everyone
       </Text>
       <Input placeholder="Search..." my="4" />
