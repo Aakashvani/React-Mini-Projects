a1801a39be0578de2c0c741221f51be81af98044
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-08-13 17:51:57 +0530

    init practice app

---
+++ usestate-array-use/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      hello world kaise haan aap
+    </div>
+  )
+}
+
+export default App
