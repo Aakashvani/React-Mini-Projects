a0d7079205e19d7d543e36743693949abf0f433d
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-08-14 16:17:15 +0530

    this is how to work with array using useState hook

--- usestate-array-use/src/App.jsx
+++ usestate-array-use/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let listOfTask = [
+    {
+      id: 0,
+      task: "learn react.js",
+    },
+    {
+      id: 2,
+      task: "learn vue.js",
+    },
+    {
+      id: 3,
+      task: "learn dancing",
+    },
+  ];
+
+  const [MyTask, setMyTask] = useState(listOfTask);
+
+  const DeleteTask = () => {
+    setMyTask([]);
+  };
+  console.log(listOfTask);
 
   return (
     <div className="App">
-      hello world kaise haan aap
+      <h2 style={{ textDecoration: "underline" }}>ToDo - App</h2>
+      {MyTask.map((e) => {
+        return <h3 key={e.id}>{e.task}</h3>;
+      })}
+      <button style={{ marginLeft: "2rem" }} onClick={DeleteTask}>
+        click
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
