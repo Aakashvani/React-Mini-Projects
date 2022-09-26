8c11b753928a0fa8a2605c3e47e4411b908c5321
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-09-27 00:47:09 +0530

    Make folder struture and make routers

---
+++ ecommerce-app/src/routers/Routers.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// importing pages
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import ProductDetails from "../pages/ProductDetails";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="Checkout" element={<Checkout />} />
+      <Route path="Login" element={<Login />} />
+      <Route path="Signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default Routers;
