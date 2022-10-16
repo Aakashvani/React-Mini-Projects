0645562c61caf4802a7377e56529306aeb123250
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-10-16 19:38:28 +0530

    Deployed on vercel 🚀

--- travel-booking-app/src/components/Carousel.jsx
+++ travel-booking-app/src/components/Carousel.jsx
@@ -11,6 +11,18 @@ const sliderData = [
   {
     url: "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
   },
+  {
+    url: "https://images.unsplash.com/photo-1644075902898-a6fba3a9b011?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlYWNoZXMlMjBpbWFnZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1604795345865-a08413457dc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2hlcyUyMG9mJTIwYXVzdHJhbGlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1659081480496-3e7caebfb3a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YmVhY2hlcyUyMG9mJTIwYXVzdHJhbGlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1512533950509-505f6af353de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlYWNoZXMlMjBvZiUyMGF1c3RyYWxpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+  },
 ];
 
 const Carousel = () => {
