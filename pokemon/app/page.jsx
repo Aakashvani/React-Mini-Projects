68266cb21570914a59d8723f9ec9ce30799c58e4
Aakash Kumar <95979529+aakashvani@users.noreply.github.com>
2024-03-26 00:28:45 +0530

    create initial states

--- pokemon/app/page.jsx
+++ pokemon/app/page.jsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import { useGlobalContext } from "@/context/global";
 
 export default function Home() {
+  const g = useGlobalContext();
+  console.log(g);
   return <div>Hello</div>;
 }
--- pokemon/context/global.js
+++ pokemon/context/global.js
@@ -1,10 +1,18 @@
 "use client";
 
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const GlobalContext = createContext();
 
+// reducer
+const reducer = (state, action) => {
+  return state;
+};
+
 export const GlobalProvider = ({ children }) => {
+  // base url
+  const baseUrl = "https://pokeapi.co/api/v2/";
+
   const initialState = {
     allPokemon: [],
     pokemon: {},
@@ -14,11 +22,25 @@ export const GlobalProvider = ({ children }) => {
     loading: false,
   };
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const allPokemon = async () => {
+    const res = await fetch(`${baseUrl}pokemon?limit=20`);
+    const data = await res.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    allPokemon();
+  }, []);
+
   return (
-    <GlobalContext.Provider value={{ ...initialState }}>
+    <GlobalContext.Provider value={{ ...state }}>
       {children}
     </GlobalContext.Provider>
   );
 };
 
-export default GlobalContext;
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
